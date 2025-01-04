47390b6581d517b988dff281655f98d8ef417926
Piiotr <ptrmkaminski@gmail.com>
2025-01-04 20:39:17 +0000

    tlcl update

---
+++ C_Primer/1_rectangle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int length, width, result;
+    printf("Enter length: ");
+    printf("Enter width: ");
+    result = length * width;
+    printf("Area = %d\n", result);
+    return 0;
+}
--- C_Primer/test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main() {
-    printf("TEST!\n");
-    printf("Next line\n");
-    return 0;
-}
